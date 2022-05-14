32302409f8467d3ede7ad52336b5626a1bf5bed1
Andre Santiago <andre.santiago@agi.com.br>
2022-05-14 17:18:31 -0300

    feat: add hello world message

--- src/main/java/com/decoderprojectead/authuser/AuthuserApplication.java
+++ src/main/java/com/decoderprojectead/authuser/AuthuserApplication.java
@@ -8,6 +8,6 @@ public class AuthuserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthuserApplication.class, args);
+		System.out.println("Hello World");
 	}
-
 }
